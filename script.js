f7b09e4b28d65d10c68d7628a8445f808c43b968
Cecilia Hallerby <cecilia.hallerby@hyperisland.se>
2020-12-08 12:43:48 +0100

    many changes

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-headerVideo = document.getElementById("hero-wrapper-video");
+const headerVideo = document.getElementById("hero-wrapper-video");
+const carosell = document.querySelector(".what-others-said");
 
 function setPlaySpeed() { 
     headerVideo.playbackRate = 0.4;
 };
 
 setPlaySpeed();
+
+// var flkty = new Flickity( carosell, {
+//   cellAlign: 'center',
+//   autoPlay: true,
+//   contain: true
+// });
